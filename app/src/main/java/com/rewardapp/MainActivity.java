5285d9715d61ec94c124d316daf028b659a3ec42
sponsorshipcollaboration63-ship-it <sponsorshipcollaboration63@gmail.com>
2025-10-26 16:54:44 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/rewardapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.rewardapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.JavascriptInterface;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.LoadAdError;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private RewardedAd rewardedAd;
+    private static final String AD_UNIT_ID = "ca-app-pub-2761605268905056/3704425417";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        MobileAds.initialize(this, initializationStatus -> {});
+        loadRewardedAd();
+        
+        webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.addJavascriptInterface(new WebAppInterface(), "Android");
+        
+        setContentView(webView);
+        webView.loadUrl("file:///android_asset/reward.html");
+    }
+    
+    private void loadRewardedAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        RewardedAd.load(this, AD_UNIT_ID, adRequest, 
+            new RewardedAdLoadCallback() {
+                @Override
+                public void onAdLoaded(RewardedAd ad) {
+                    rewardedAd = ad;
+                }
+                
+                @Override
+                public void onAdFailedToLoad(LoadAdError loadAdError) {
+                    rewardedAd = null;
+                }
+            });
+    }
+    
+    private void showRewardedAd() {
+        if (rewardedAd != null) {
+            rewardedAd.show(this, rewardItem -> {
+                webView.evaluateJavascript("rewardUser(0.01)", null);
+                loadRewardedAd();
+            });
+        }
+    }
+    
+    public class WebAppInterface {
+        @JavascriptInterface
+        public void showAd() {
+            runOnUiThread(() -> showRewardedAd());
+        }
+    }
+}
